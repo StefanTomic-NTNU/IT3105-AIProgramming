76163dcae82ae6df8cf9561f0a9f0d2f697a7902
Stefan Djordje Tomic <stefandt@stud.ntnu.no>
2022-03-04 15:34:32 +0100

    Decent print

--- simworld/hex_board.py
+++ simworld/hex_board.py
@@ -4,6 +4,7 @@ import numpy as np
 class Cell:
     def __init__(self):
         self.neighbors = []
+        self.piece = None
 
 
 class Hex:
@@ -29,18 +30,44 @@ class Hex:
             pass
 
     def render(self):
-        print_list = []
-        for r in range(self.size):
-            print_row = ''
-            for c in range(self.size):
-                print_row += '*\t'
 
-            print_list.append(print_row)
+        print_list = []
+
+        for row in range(self.size * 3 + 1):
+            print_list.append('')
+
+        for i in range(len(print_list)):
+            print_list[i] += '  ' * (abs(round(len(print_list)/2 - i)))
+
+        print(f'Print List length: {len(print_list)}')
+        for r in range(self.size):
+            for c in range(self.size):
+                print_index = (r + c) * 2
+                print(print_index)
+
+                if '*' in print_list[print_index]:
+                    print_list[print_index] += '----- * '
+                else:
+                    print_list[print_index] += ' * '
+
+
 
         for row in print_list:
             print(row)
 
 
+        # print_list = []
+        # for r in range(self.size):
+        #     print_row = ''
+        #     for c in range(self.size):
+        #         print_row += '*\t'
+        #
+        #     print_list.append(print_row)
+        #
+        # for row in print_list:
+        #     print(row)
+
+
 if __name__ == '__main__':
-    hex_board = Hex(5)
+    hex_board = Hex(4)
     hex_board.render()
