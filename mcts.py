823b1364c7dbfcba81c6d18c03482802e31c2743
Stefan Djordje Tomic <stefandt@stud.ntnu.no>
2022-03-19 15:06:42 +0100

    Search implemented MCTS

--- mcts.py
+++ mcts.py
@@ -1,14 +1,21 @@
+import copy
+import random
+import numpy as np
+
 from simworld.nim import Nim
 
 
 class TreeNode:
     def __init__(self, state: dict):
         self.parent = None
+        self.score_a = []
+        self.N_a = []
+        self.Q_a = []
         self.edges = []
         self.children = []
-        self.state = state
+        self.state = copy.copy(state)
         self.score = 0
-        self.N = 0
+        self.N = 1
         self.Q = 0
 
 
@@ -21,18 +28,17 @@ class MCTS:
     def run(self):
         for g_a in range(self.number_actual_games):
             board_a = self.game
-            init_state = board_a.state
-            root = TreeNode(init_state)
+            root = TreeNode(copy.copy(board_a.state))
             while not board_a.is_game_over():
                 board_mc = self.game
-                board_mc.state = root.state
+                board_mc.state = copy.copy(root.state)
 
                 for g_s in range(self.number_search_games):
                     node = root
                     while len(node.children) != 0:
-                        traversing_node = node.children[0]  # TODO: Replace tree policy
+                        traversing_node = node.children[self.tree_policy(node)]  # TODO: Replace tree policy
                         node = traversing_node
-                        board_mc.state = traversing_node.state
+                        board_mc.state = copy.copy(traversing_node.state)
                     self.generate_children(node)
                     # Rollout:
                     rollout_nodes = []
@@ -40,8 +46,9 @@ class MCTS:
                     while not board_mc.is_game_over():
                         self.generate_children(node)
                         # TODO: Select action using ANET
-                        board_mc.make_move(node.edges[0])
-                        node = TreeNode(board_mc.state)
+                        chosen_action = random.randrange(len(node.edges))
+                        board_mc.make_move(node.edges[chosen_action])
+                        node = node.children[chosen_action]
                         rollout_nodes.append(node)
                     if board_mc.state['pid'] == 1:
                         eval = -1
@@ -50,9 +57,15 @@ class MCTS:
                     node = rollout_nodes[0]
                     parent = node.parent
                     while parent:
+                        # print('parent')
                         node.score += eval
                         node.N += 1
                         node.Q = node.score / node.N
+                        edge_index = node.parent.children.index(node)
+                        node.parent.score_a[edge_index] += eval
+                        node.parent.N_a[edge_index] += 1
+                        node.parent.Q_a[edge_index] = node.parent.score_a[edge_index] / node.parent.N_a[edge_index]
+                        node = node.parent
                         parent = node.parent
                 # TODO: Get D   ;)
                 action = root.edges[0]  # TODO: Choose action based on D
@@ -61,13 +74,22 @@ class MCTS:
                 root.parent = None
 
     def generate_children(self, tree_node: TreeNode):
-        edges, states = self.game.generate_children_(tree_node.state)
-        children = [TreeNode(child) for child in states]
-        for i in range(len(children)):
-            self.add_child(tree_node, children[i], edges[i])
+        if len(tree_node.children) == 0:
+            edges, states = self.game.generate_children_(tree_node.state)
+            children = [TreeNode(child) for child in states]
+            for i in range(len(children)):
+                self.add_child(tree_node, children[i], edges[i])
 
     def add_child(self, parent, child, edge):
         if child not in parent.children:
             parent.children.append(child)
             parent.edges.append(edge)
+            parent.N_a.append(1)    # TODO: to avoid 0 log
+            parent.score_a.append(0)
+            parent.Q_a.append(0)
             child.parent = parent
+
+    def tree_policy(self, node: TreeNode):
+        u = [1*np.sqrt(np.log(node.N)/(1 + N_sa)) for N_sa in node.N_a]
+        combined = np.add(u, node.Q_a)
+        return np.argmax(combined)
